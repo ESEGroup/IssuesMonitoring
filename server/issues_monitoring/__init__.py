1859a9b426e2a0183a322924673bcdbe1c3727cc
Yago Meira Lopes de Carvalho <yago.meira@poli.ufrj.br>
2017-04-22 23:14:20 -0300

    Fix hello world

--- server/issues_monitoring/__init__.py
+++ server/issues_monitoring/__init__.py
@@ -3,5 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 
 from ..config import Config
-from os       import getenv
-app.config['SECRET_KEY'] = getenv('SECRET_KEY') or Config.secret_key
+from .views import Views
--- server/issues_monitoring/views.py
+++ server/issues_monitoring/views.py
@@ -1,7 +1,8 @@
 from ..issues_monitoring import app, Config
 from flask import render_template, request, redirect, url_for, session
 
-@app.route('/')
-def hello_world():
-    print("Entrou")
-    return 'Hello, World!'
+class Views:
+    @app.route('/')
+    def hello_world():
+        print("Entrou")
+        return 'Hello, World!'
