313c116e5b7937430332bc008d21ab31f70985c4
Paulo Cabral Sanz <paulocabral@sanz.com.br>
2017-04-29 03:07:07 -0300

    Clean db access script

--- server/db/db.py
+++ server/db/db.py
@@ -8,21 +8,34 @@ class DB:
         conn = sqlite3.connect(self.name)
         return conn
 
-    def execute(self, query, arguments=tuple(), return_id=False):
-        pk = None
+    def __execute(self, function, query, arguments, return_id, fetch):
+        conn = self.connect()
+        cursor = conn.cursor()
+
+        sqlite3.Cursor.__dict__[function](cursor, query, arguments)
+        conn.commit()
+
+        if fetch is not None:
+            data = sqlite3.Cursor.__dict__[fetch](cursor)
+        elif return_id:
+            cursor.execute("SELECT last_insert_rowid();")
+            data = cursor.fetchone()[0]
+        else:
+            data = None
+
+        conn.close()
+        return data
+
+    def __retry(self, function, query, arguments, return_id=None, fetch=None):
         _tries = 2
         while(_tries > 0):
             try:
-                conn = self.connect()
-                cursor = conn.cursor()
-                cursor.execute(query, arguments)
-
-                if return_id:
-                    cursor.execute("SELECT last_insert_rowid();")
-                    pk = cursor.fetchone()[0]
-
-                conn.commit()
-                conn.close()
+                pk = DB.__execute(self,
+                                  function,
+                                  query,
+                                  arguments,
+                                  return_id,
+                                  fetch)
                 _tries = 0
             except sqlite3.Error:
                 _tries -= 1
@@ -31,49 +44,22 @@ class DB:
 
         return pk
 
+    def execute(self, query, arguments=tuple(), return_id=False):
+        return DB.__retry(self, "execute", query, arguments, return_id)
+
     def executemany(self, query, list_of_arguments=[tuple()]):
-        _tries = 2
-        while(_tries > 0):
-            try:
-                conn = self.connect()
-                cursor = conn.cursor()
-                cursor.executemany(query, list_of_arguments)
-                conn.commit()
-                conn.close()
-                _tries = 0
-            except sqlite3.Error:
-                _tries -= 1
-                if _tries == 0:
-                    raise
+        DB.__retry(self, "executemany", query, list_of_arguments)
 
     def fetchone(self, query, arguments=tuple()):
-        _tries = 2
-        while(_tries > 0):
-            try:
-                conn = self.connect()
-                cursor = conn.cursor()
-                cursor.execute(query, arguments)
-                data = cursor.fetchone()
-                conn.close()
-                _tries = 0
-            except sqlite3.Error:
-                _tries -= 1
-                if _tries == 0:
-                    raise
-        return data
+        return DB.__retry(self,
+                          "execute",
+                          query,
+                          arguments,
+                          fetch="fetchone")
 
     def fetchall(self, query, arguments=tuple()):
-        _tries = 2
-        while(_tries > 0):
-            try:
-                conn = self.connect()
-                cursor = conn.cursor()
-                cursor.execute(query, arguments)
-                data = cursor.fetchall()
-                conn.close()
-                _tries = 0
-            except sqlite3.Error:
-                _tries -= 1
-                if _tries == 0:
-                    raise
-        return data
+        return DB.__retry(self,
+                          "execute",
+                          query,
+                          arguments,
+                          fetch="fetchall")
