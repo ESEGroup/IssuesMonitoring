f8d7f1d9036750b17a2e4741e2176b42e0c130f4
Eduardo <eduardo-castanho@hotmail.com>
2017-05-17 14:41:49 -0300

    machine temperature implemented on main routine

--- arduino/src/IssuesMonitoring/IssuesMonitoring.ino
+++ arduino/src/IssuesMonitoring/IssuesMonitoring.ino
@@ -2,6 +2,8 @@
 #include "timer.h"
 #include "timerManager.h"
 #include "MathBox.h"
+#include "OneWire.h"
+#include "DallasTemperature.h"
 
 // Pins
 const byte LDR_PIN = 2;
@@ -13,11 +15,19 @@ DHT dht(DHT_PIN, DHT11);
 // Timers
 Timer ldr_timer;
 Timer dht_timer;
+Tomer temp_timer;
 Timer send_timer;
+
+// Temperature Sensors
+#define ONE_WIRE_BUS 3
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire,);
+DeviceAddress sensor1, sensor2;
 
 // Intervals [ms]
 unsigned int ldr_interval = 3000;
 unsigned int dht_interval = 5000;
+unsigned int temp_interval = 5000;
 unsigned int send_interval = 15000;
 
 // Signal processing
@@ -25,6 +35,8 @@ MathBox ldr_signal;
 MathBox temperature_signal;
 MathBox humidity_signal;
 MathBox heat_index_signal;
+MathBox machine1_temperature_signal;
+MathBox machine2_temperature_signal;
 
 // Print functions
 void printLDR(byte ldr) {
@@ -43,6 +55,15 @@ void printDHT(float humidity, float temperature, float heat_index) {
   Serial.print("Heat index: ");
   Serial.print(heat_index);
   Serial.println(" *C ");
+  Serial.println("");
+}
+
+void printTemp(int id, float temperature) {
+  Serial.print("Sensor ");
+  Serial.print(id);
+  Serial.print(": ");
+  Serial.print(temperature);
+  Serial.println(" *C");
   Serial.println("");
 }
 
@@ -75,6 +96,16 @@ void readDHT() {
   printDHT(humidity, temperature, heat_index);
 }
 
+void readTemp() {
+  sensors.requestTemperatures();
+  float tempC1 = sensors.getTempC(sensor1);
+  float tempC2 = sensors.getTempC(sensor2);
+  machine1_temperature_signal.add(tempC1);
+  machine2_temperature_signal.add(tempC2);
+  printTemp(1, tempC1);
+  printTemp(2, tempC2);
+}
+
 void send() {
   Serial.println("==== Send ====");
 
@@ -86,11 +117,15 @@ void send() {
     temperature_signal.getAverage(),
     heat_index_signal.getAverage()
   );
+  printTemp(1,machine1_temperature_signal.getAverage());
+  printTemp(2,machine2_temperature_signal.getAverage());
 
   ldr_signal.clear();
   humidity_signal.clear();
   temperature_signal.clear();
   heat_index_signal.clear();
+  machine1_temperature_signal.clear();
+  machine2_temperature_signal.clear();
 }
 
 void setup() {
@@ -105,11 +140,18 @@ void setup() {
   // Timer setup
   ldr_timer.setInterval(ldr_interval);
   dht_timer.setInterval(dht_interval);
+  temp_timer.setInterval(temp_interval);
   send_timer.setInterval(send_interval);
 
   ldr_timer.setCallback(readLDR);
   dht_timer.setCallback(readDHT);
+  temp_timer.setCallback(readTemp);
   send_timer.setCallback(send);
+
+  //Temperature sensors setup
+  sensors.begin();
+  sensors.getAddress(sensor1, 0);
+  sensors.getAddress(sensor2, 1);
 
   TimerManager::instance().start();
 }
@@ -117,4 +159,3 @@ void setup() {
 void loop() {
   TimerManager::instance().update();
 }
-
