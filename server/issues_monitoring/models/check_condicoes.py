0ef9c9380edc2eca93eb685f2381b299e6e6a091
debora <deborabpina@gmail.com>
2017-05-17 23:16:43 -0300

    notify users and admin if environment conditions are outside the comfort zone

--- server/issues_monitoring/models/check_condicoes.py
+++ server/issues_monitoring/models/check_condicoes.py
@@ -39,3 +39,66 @@ Pedimos que procure uma solução quanto a isso, para evitar o gasto desnecessá
             emails = [a.email for a in admins]
             #if it got here, just send message to supervisor(s)
             send_email(subject, msgContent, emails)
+
+
+def CheckForEnvironmentConditions():
+    data = db.fetchall("""
+        SELECT temp_min, temp_max, umid_min, umid_max 
+        FROM Zona_de_Conforto_Lab;""")
+    umid_min=0
+    umid_max=0
+    temp_min=0
+    temp_max=0
+
+    for row in data:
+        temp_min=row[0]
+        temp_max=row[1]
+        umid_min=row[2]
+        umid_max=row[3]
+
+    current_temp = 0
+    current_umid = 0
+    data = db.fetchall("""SELECT temp, umid 
+                          FROM Log_Lab ORDER BY data DESC LIMIT 1;""")
+    
+    for row in data:
+        current_temp = row[0]
+        current_umid = row[1]
+    
+    if (current_temp < temp_min or current_temp > temp_max):
+        subject = "Aviso de temperatura anormal"
+        msgContent = """
+Caro responsável,
+Você está recebendo essa mensagem pois a temperatura do laboratorio se encontra fora da zona de conforto.
+Pedimos que procure uma solução quanto a isso.
+\n\nAtenciosamente, \nEquipe ISSUES Monitoring"""
+
+        data = db.fetchall("""
+            SELECT u.email
+            FROM Presenca p
+            INNER JOIN User_Lab u
+              ON p.user_id = u.user_id
+            WHERE presente = 1;""")
+
+        admins = AdministradorSistema.obter_administradores()
+        emails = [a.email for a in admins]
+        if len(data) != 0:
+            for d in data:
+                emails += [d[0]]    
+
+        #if it got here, just send message to supervisor(s)
+        send_email(subject, msgContent, emails)
+        
+    if (current_umid < umid_min or current_umid > umid_max):
+        subject = "Aviso de umidade anormal"
+        msgContent = """
+Caro responsável,
+Você está recebendo essa mensagem pois a umidade do laboratorio se encontra fora da zona de conforto.
+Pedimos que procure uma solução quanto a isso.
+\n\nAtenciosamente, \nEquipe ISSUES Monitoring"""
+
+        admins = AdministradorSistema.obter_administradores()
+        emails = [a.email for a in admins]
+
+        #if it got here, just send message to supervisor(s)
+        send_email(subject, msgContent, emails)     
