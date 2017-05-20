086f0ecb7cca44020dc0b34653d7ad3f497c6fc6
debora <deborabpina@gmail.com>
2017-05-19 22:58:59 -0300

    check conditions for different labs

--- server/issues_monitoring/models/check_condicoes.py
+++ server/issues_monitoring/models/check_condicoes.py
@@ -10,10 +10,10 @@ from ..models import  AdministradorSistema
 from . import db
 
 #checks if lights were left on while no one was in lab
-def CheckForForgottenLights():
+def CheckForForgottenLights(lab_id):
     #checks for people in lab
     data = db.fetchall("""SELECT User_Lab.email 
-                          FROM User_Lab, Presenca WHERE User_Lab.user_id=Presenca.user_id AND Presenca.presente=1;""")
+                          FROM User_Lab, Presenca WHERE User_Lab.user_id = Presenca.user_id AND Presenca.presente=1 AND Presenca.lab_id = ?;""", (lab_id,))
     presentUsers =[]
     for row in data:
         presentUsers.append(str(row[0]))
@@ -21,7 +21,7 @@ def CheckForForgottenLights():
     if(len(presentUsers)==0):
         #no one is in the lab, checks if lights are on
         data = db.fetchall("""SELECT lum 
-                              FROM Log_Lab ORDER BY data DESC LIMIT 1;""")
+                              FROM Log_Lab WHERE lab_id = ? ORDER BY data DESC LIMIT 1;""", (lab_id,))
         lightsOn=False
         for row in data:
             lightsOn=row[0]
@@ -31,7 +31,7 @@ def CheckForForgottenLights():
             subject = "Aviso de luz acesa"
             msgContent = """
 Caro responsável,
-Você está recebendo essa mensagem pois a luz do laboratorio foi deixada acesa e não há mais funcionários presentes.
+Você está recebendo essa mensagem pois a luz do laboratorio """ + str(lab_id) + """ foi deixada acesa e não há mais funcionários presentes.
 Pedimos que procure uma solução quanto a isso, para evitar o gasto desnecessário de energia.
 \n\nAtenciosamente, \nEquipe ISSUES Monitoring"""
     
@@ -41,10 +41,12 @@ Pedimos que procure uma solução quanto a isso, para evitar o gasto desnecessá
             send_email(subject, msgContent, emails)
 
 
-def CheckForEnvironmentConditions():
+def CheckForEnvironmentConditions(lab_id):
     data = db.fetchall("""
         SELECT temp_min, temp_max, umid_min, umid_max 
-        FROM Zona_de_Conforto_Lab;""")
+        FROM Zona_de_Conforto_Lab z, Lab l 
+        WHERE z.zona_conforto_id = l.zona_conforto_id AND lab_id = ?""", (lab_id,))
+
     umid_min=0
     umid_max=0
     temp_min=0
@@ -59,7 +61,7 @@ def CheckForEnvironmentConditions():
     current_temp = 0
     current_umid = 0
     data = db.fetchall("""SELECT temp, umid 
-                          FROM Log_Lab ORDER BY data DESC LIMIT 1;""")
+                          FROM Log_Lab WHERE lab_id = ? ORDER BY data DESC LIMIT 1; """, (lab_id,))
     
     for row in data:
         current_temp = row[0]
@@ -69,7 +71,7 @@ def CheckForEnvironmentConditions():
         subject = "Aviso de temperatura anormal"
         msgContent = """
 Caro responsável,
-Você está recebendo essa mensagem pois a temperatura do laboratorio se encontra fora da zona de conforto.
+Você está recebendo essa mensagem pois a temperatura do laboratorio """ + str(lab_id) + """ se encontra fora da zona de conforto.
 Pedimos que procure uma solução quanto a isso.
 \n\nAtenciosamente, \nEquipe ISSUES Monitoring"""
 
@@ -78,13 +80,14 @@ Pedimos que procure uma solução quanto a isso.
             FROM Presenca p
             INNER JOIN User_Lab u
               ON p.user_id = u.user_id
-            WHERE presente = 1;""")
+            WHERE presente = 1 AND lab_id = ?; """, (lab_id,))
 
         admins = AdministradorSistema.obter_administradores()
         emails = [a.email for a in admins]
         if len(data) != 0:
             for d in data:
                 emails += [d[0]]    
+        print (emails)
 
         #if it got here, just send message to supervisor(s)
         send_email(subject, msgContent, emails)
@@ -93,12 +96,23 @@ Pedimos que procure uma solução quanto a isso.
         subject = "Aviso de umidade anormal"
         msgContent = """
 Caro responsável,
-Você está recebendo essa mensagem pois a umidade do laboratorio se encontra fora da zona de conforto.
+Você está recebendo essa mensagem pois a umidade do laboratorio """ + str(lab_id) + """  se encontra fora da zona de conforto.
 Pedimos que procure uma solução quanto a isso.
 \n\nAtenciosamente, \nEquipe ISSUES Monitoring"""
 
+        data = db.fetchall("""
+            SELECT u.email
+            FROM Presenca p
+            INNER JOIN User_Lab u
+              ON p.user_id = u.user_id
+            WHERE presente = 1 AND lab_id = ?; """, (lab_id,))
+
         admins = AdministradorSistema.obter_administradores()
         emails = [a.email for a in admins]
+        if len(data) != 0:
+            for d in data:
+                emails += [d[0]]    
 
+        print (emails)
         #if it got here, just send message to supervisor(s)
         send_email(subject, msgContent, emails)     
