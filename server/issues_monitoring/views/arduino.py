d1a4f37dd2c773a6c7a11636e013068ee68327dc
Yago Meira Lopes de Carvalho <yago.meira@poli.ufrj.br>
2017-05-21 00:02:58 -0300

    Fix check-lights

--- server/issues_monitoring/views/arduino.py
+++ server/issues_monitoring/views/arduino.py
@@ -24,15 +24,14 @@ def arduino():
 
 @app.route('/arduino', methods=['GET'])
 def arduino_get():
-    json_str = request.args.get("json","")
-    j = json.loads(json_str)
-    if j is not None:
+    json_str = request.args.get("json",None)
+    if json_str is not None:
+        j = json.loads(json_str)
         try:
             if (controllers.registrar_medidas(j)):
                 lab_id = j["lab_id"]
                 return str(Laboratorio.obter_intervalo_arduino(lab_id))
             else:
-                print("RETORNOU FALSEE")
                 return "-1"
         except sqlite3.Error:
             return "-2"
