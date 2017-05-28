ff470445e741d38b5e5dbe08df8e15011b27d71e
carloseduardov8 <carloseduardov8@hotmail.com>
2017-05-28 15:38:31 -0300

    Add slider for humidity

--- new_layout/labs.js
+++ new_layout/labs.js
@@ -36,7 +36,7 @@ rangeSlider();
 //JS para os sliders de dois handles presentes em cadastrar lab:
 
 $( function() {
-    $( "#slider-range" ).slider({
+    $( "#slider-temp" ).slider({
         range: true,
         min: 10,
         max: 50,
@@ -45,5 +45,18 @@ $( function() {
             $( "#temp-lab" ).val( ui.values[ 0 ] + "ºC a " + ui.values[ 1 ] + "ºC" );
         }
     });
-    $( "#temp-lab" ).val($( "#slider-range" ).slider( "values", 0 ) + "ºC a " + $( "#slider-range" ).slider( "values", 1 ) + "ºC");
+    $( "#temp-lab" ).val($( "#slider-temp" ).slider( "values", 0 ) + "ºC a " + $( "#slider-temp" ).slider( "values", 1 ) + "ºC");
+} );
+
+$( function() {
+    $( "#slider-umidade" ).slider({
+        range: true,
+        min: 0,
+        max: 100,
+        values: [ 20, 30 ],
+        slide: function( event, ui ) {
+            $( "#umidade-lab" ).val( ui.values[ 0 ] + "% a " + ui.values[ 1 ] + "%" );
+        }
+    });
+    $( "#umidade-lab" ).val($( "#slider-umidade" ).slider( "values", 0 ) + "% a " + $( "#slider-umidade" ).slider( "values", 1 ) + "%");
 } );
