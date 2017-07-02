12f8667ea11876e435717946bc06a171c20e0007
Paulo Cabral Sanz <paulocabral@sanz.com.br>
2017-07-02 15:47:11 -0300

    Re-raise erros in debug to make debugging easier

--- server/issues_monitoring/views/errors.py
+++ server/issues_monitoring/views/errors.py
@@ -1,14 +1,11 @@
 import sqlite3
 from flask import redirect, url_for, render_template
 from ..common.utils import autenticado, admin_autenticado
-from .. import app
+from .. import app, Config
 
 @app.errorhandler(sqlite3.Error)
 def db_error(error):
-    print(error)
-    return render_template("500.html",
-                            admin=admin_autenticado(),
-                            autenticado=autenticado()) , 500
+    return server_error(error)
 
 @app.errorhandler(404)
 def not_found_error(error):
@@ -17,8 +14,11 @@ def not_found_error(error):
                             autenticado=autenticado()) , 404
 
 @app.route('/500')
+@app.errorhandler(Exception)
 @app.errorhandler(500)
-def server_error():
+def server_error(error):
+    if Config.debug:
+        raise error
     return render_template("500.html",
                             admin=admin_autenticado(),
                             autenticado=autenticado()) , 500
